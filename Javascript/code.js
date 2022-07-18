1af0b28b018c9ae8f3fd7d18f0d1068bed70a117
Cristiannm01 <cristiannm01@gmail.com>
2022-07-17 22:26:26 -0500

    Corregido descarga imagenes

--- Javascript/code.js
+++ Javascript/code.js
@@ -1,29 +1,21 @@
 
 var url = "https://picsum.photos/500/500";
 var imagenOriginal
-var nameDownload = 'image.jpg'
-var color = ""
 function getImage() {
 
-    fetch(url).then(resp => resp.blob())
-        .then(blob => {
-            url = window.URL.createObjectURL(blob)
-            const a = document.createElement('a')
-            a.style.display = 'none'
-            a.href = url
-            a.download = nameDownload
-            document.body.appendChild(a)
-            a.click()
-            window.URL.revokeObjectURL(url)
-        })
-        .catch(() => alert('error'))
-
-    imagenOriginal = document.getElementById('imagen')
+    fetch(url).then(response => {
+        console.log(response);
+        imagen.src= response.url;
+        }).catch(error => {
+        console.log(error);
+        alert('Ha ocurrido un error!');
+        });
 }
 
 function aplicarFiltro(filter) {
     imagenOriginal = document.getElementById("imagen")
     var imagenFiltro = document.getElementById("canvasFiltro")
+    imagen.crossOrigin = "Anonymous"
     filter = LenaJS[filter]
     LenaJS.filterImage(imagenFiltro, filter, imagenOriginal)
 }
